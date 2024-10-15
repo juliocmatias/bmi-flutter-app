9edda4e163e11deddc19fe4a9d7e0c9167fcd3be
Julio Matias <juliocesark2@hotmail.com>
2024-10-15 20:33:56 -0300

    data: commit initial

---
+++ android/app/src/main/kotlin/com/juliocmatias/bmiapp/bmiapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.juliocmatias.bmiapp.bmiapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:bmiapp/pages/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Login());
+}
