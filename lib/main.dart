2351952b6144afbe7189a2a7c84ea9415e9620b9
Julio Matias <juliocesark2@hotmail.com>
2024-10-28 23:01:24 -0300

    feat: Implement global store with provider in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:bmiapp/my_app.dart';
+import 'package:bmiapp/stores/login_store.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => LoginStore(),
+      child: const MyApp(),
+    ),
+  );
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,7 @@
 import 'package:bmiapp/pages/main_page.dart';
 import 'package:bmiapp/stores/login_store.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,9 +13,10 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   var emailController = TextEditingController(text: 'email@email.com');
   var passwordController = TextEditingController(text: '123');
-  final store = LoginStore();
 
   void navigatorPage() async {
+    final store = context.read<LoginStore>();
+
     await store.logging(emailController.text, passwordController.text);
 
     if (mounted) {
@@ -25,29 +27,34 @@ class _LoginState extends State<Login> {
             builder: (context) => const MainPage(),
           ),
         );
+      } else if (store.state.erro.isNotEmpty) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          showAlertDialog('Erro no Login', store.state.erro);
+          store.clearError();
+        });
       }
     }
   }
 
   void showAlertDialog(String title, String message) {
     showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              backgroundColor:
-                  Theme.of(context).colorScheme.inversePrimary.withOpacity(0.6),
-              title: Text(
-                title,
-                style: const TextStyle(color: Colors.white),
-              ),
-              content:
-                  Text(message, style: const TextStyle(color: Colors.white)),
-              actions: [
-                TextButton(
-                    onPressed: () => Navigator.pop(context, 'OK'),
-                    child:
-                        const Text('Ok', style: TextStyle(color: Colors.white)))
-              ],
-            ));
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor:
+            Theme.of(context).colorScheme.inversePrimary.withOpacity(0.6),
+        title: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
+        content: Text(message, style: const TextStyle(color: Colors.white)),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('Ok', style: TextStyle(color: Colors.white)),
+          )
+        ],
+      ),
+    );
   }
 
   @override
@@ -55,97 +62,91 @@ class _LoginState extends State<Login> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Theme.of(context).colorScheme.onSurface,
-        body: ListenableBuilder(
-            listenable: store,
-            builder: (context, child) {
-              if (store.state.erro.isNotEmpty) {
-                WidgetsBinding.instance.addPostFrameCallback((_) {
-                  showAlertDialog('Error Login', store.state.erro);
-                  store.clearError();
-                });
-              }
+        body: Consumer<LoginStore>(
+          builder: (context, store, child) {
+            return LayoutBuilder(
+              builder: (context, constraints) {
+                double columnWidth =
+                    constraints.maxWidth > 600 ? 400 : double.infinity;
+                double padding = constraints.maxWidth > 600 ? 40.0 : 19.0;
+                double paddingButton = constraints.maxWidth > 600 ? 70.0 : 19.0;
 
-              return LayoutBuilder(
-                builder: (context, constraints) {
-                  double columnWidth =
-                      constraints.maxWidth > 600 ? 400 : double.infinity;
-                  double padding = constraints.maxWidth > 600 ? 40.0 : 19.0;
-                  double paddingButton =
-                      constraints.maxWidth > 600 ? 70.0 : 19.0;
+                if (store.state.isLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                }
 
-                  if (store.state.isLoading) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  return SingleChildScrollView(
-                    child: IntrinsicHeight(
-                      child: Center(
-                        child: SizedBox(
-                          width: columnWidth,
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Expanded(child: Container()),
-                                  Expanded(
-                                    flex: 12,
-                                    child: Image.asset('images/logo-imc.png'),
-                                  ),
-                                  Expanded(child: Container())
-                                ],
-                              ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(
-                                    padding, 0, padding, 19),
-                                child: TextField(
-                                  controller: emailController,
-                                  style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.secondary,
-                                  ),
-                                  decoration: const InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: "Email",
-                                  ),
+                return SingleChildScrollView(
+                  child: IntrinsicHeight(
+                    child: Center(
+                      child: SizedBox(
+                        width: columnWidth,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(child: Container()),
+                                Expanded(
+                                  flex: 12,
+                                  child: Image.asset('images/logo-imc.png'),
+                                ),
+                                Expanded(child: Container())
+                              ],
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsets.fromLTRB(padding, 0, padding, 19),
+                              child: TextField(
+                                controller: emailController,
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                ),
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: "Email",
                                 ),
                               ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(
-                                    padding, 0, padding, 19),
-                                child: TextField(
-                                  obscureText: true,
-                                  controller: passwordController,
-                                  style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.secondary,
-                                  ),
-                                  decoration: const InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: "Senha",
-                                  ),
+                            ),
+                            Padding(
+                              padding:
+                                  EdgeInsets.fromLTRB(padding, 0, padding, 19),
+                              child: TextField(
+                                obscureText: true,
+                                controller: passwordController,
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                ),
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: "Senha",
                                 ),
                               ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(
-                                    paddingButton, 0, paddingButton, 19),
-                                child: SizedBox(
-                                  width: double.infinity,
-                                  child: FilledButton(
-                                    onPressed: () => navigatorPage(),
-                                    child: const Text("Entrar"),
-                                  ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(
+                                  paddingButton, 0, paddingButton, 19),
+                              child: SizedBox(
+                                width: double.infinity,
+                                child: FilledButton(
+                                  onPressed: () {
+                                    navigatorPage();
+                                  },
+                                  child: const Text("Entrar"),
                                 ),
                               ),
-                              Expanded(child: Container()),
-                            ],
-                          ),
+                            ),
+                            Expanded(child: Container()),
+                          ],
                         ),
                       ),
                     ),
-                  );
-                },
-              );
-            }),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
