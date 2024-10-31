9fc87ed0c012f4c2004c5afbcdff1d620e6189c1
Julio Matias <juliocesark2@hotmail.com>
2024-10-31 16:19:25 -0300

    refactor: chanse super key in mainPageView

--- lib/pages/main_page_view.dart
+++ lib/pages/main_page_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MainPageView extends StatelessWidget {
-  const MainPageView({Key? key}) : super(key: key);
+  const MainPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
